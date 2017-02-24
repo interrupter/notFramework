a1b3c42a0931198be648a268e183c70bc25985aa
interrupter <alex.repin@mail.ru>
2017-02-25 03:00:46 +0400

    v2.1.27

--- src/common/strings.js
+++ src/common/strings.js
@@ -2,6 +2,9 @@ var CommonStrings = {
 	capitalizeFirstLetter(string) {
 		return string.charAt(0).toUpperCase() + string.slice(1);
 	},
+	lowerFirstLetter(string) {
+		return string.charAt(0).toLowerCase() + string.slice(1);
+	},
 };
 
 export default CommonStrings;

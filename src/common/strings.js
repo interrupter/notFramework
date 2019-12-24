f0578df7b6451d205ad6eda8c6267110d980926b
Aleksander Repin <alex.repin@mail.ru>
2019-12-24 19:02:46 +0300

    v2.13.29

--- src/common/strings.js
+++ src/common/strings.js
@@ -5,6 +5,14 @@ var CommonStrings = {
 	lowerFirstLetter(string) {
 		return string.charAt(0).toLowerCase() + string.slice(1);
 	},
+	escapeHtml(unsafe) {
+		return unsafe
+			.replace(/&/g, '&amp;')
+			.replace(/</g, '&lt;')
+			.replace(/>/g, '&gt;')
+			.replace(/"/g, '&quot;')
+			.replace(/'/g, '&#039;');
+	}
 };
 
 export default CommonStrings;
